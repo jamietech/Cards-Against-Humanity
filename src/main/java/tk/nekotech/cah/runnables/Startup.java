95ab53370413038e2adb93c2cf9e26d382da9352
jamietech <jamie@jamietech.co.cc>
2012-10-22 19:03:18 +1000

    De-voice users on quit.

--- src/main/java/tk/nekotech/cah/runnables/Startup.java
+++ src/main/java/tk/nekotech/cah/runnables/Startup.java
@@ -3,6 +3,7 @@ package tk.nekotech.cah.runnables;
 import java.util.Scanner;
 import org.pircbotx.Channel;
 import tk.nekotech.cah.CardsAgainstHumanity;
+import tk.nekotech.cah.Player;
 
 public class Startup extends Thread {
     private final CardsAgainstHumanity cah;
@@ -18,6 +19,18 @@ public class Startup extends Thread {
         System.out.println("Shutting down...");
         final Channel channel = this.cah.spamBot.getChannel("#CAH");
         this.cah.spamBot.setTopic(channel, this.cah.topic + " | Bot currently offline.");
+        StringBuilder sb = new StringBuilder();
+        String mode = "-";
+        for (Player player : this.cah.players) {
+            if (!player.isWaiting()) {
+                sb.append(player.getName() + " ");
+                mode += "v";
+            }
+        }
+        if (sb.length() != 0) {
+            sb.delete(sb.length() - 1, sb.length());
+            this.cah.cardBot.setMode(this.cah.cardBot.getChannel("#CAH"), mode + " " + sb.toString());
+        }
         System.out.println("Nearly done!");
         try {
             Thread.sleep(1000);
