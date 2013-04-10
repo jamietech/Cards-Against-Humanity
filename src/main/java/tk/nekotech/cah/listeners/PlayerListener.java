e6aa62fde84288c1441a2d695feb511e90a89d58
infina <cbouton1@gmail.com>
2013-04-10 02:23:34 -0300

    Update PlayerListener.java

--- src/main/java/tk/nekotech/cah/listeners/PlayerListener.java
+++ src/main/java/tk/nekotech/cah/listeners/PlayerListener.java
@@ -56,7 +56,7 @@ public class PlayerListener extends MasterListener {
                         } else {
                             send.append(this.cah.blackCard.getColored().replaceFirst("_", win.getPlayedCards()[0].getColored()).replaceFirst("_", win.getPlayedCards()[1].getColored()));
                         }
-                        this.cah.spamBot.sendMessage("#CAH", send.toString());
+                        this.cah.spamBot.sendMessage("#MIASACraft", send.toString());
                         win.addPoint();
                         this.cah.nextRound();
                     }
